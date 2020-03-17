00b09d6ed7982173e89ba61b89f656115509da4c
unknown <pranavvidyarthi7@gmail.com>
2020-03-18 01:33:46 +0530

    Sign Up and Sign In Pages

---
+++ script.js
@@ -0,0 +1,219 @@
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+// Problem: User interaction doesn't provide desired results.
+// Solution: Add interactivity so the user can manage daily tasks
+
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementById("ad")[0];
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
+
+//New Task List Item
+var createNewTaskElement = function(taskString) {
+  //Create List Item
+  var listItem = document.createElement("li");
+
+  //input (checkbox)
+  var checkBox = document.createElement("input"); // checkbox
+  //label
+  var label = document.createElement("label");
+  //input (text)
+  var editInput = document.createElement("input"); // text
+  //button.edit
+  var editButton = document.createElement("button");
+  //button.delete
+  var deleteButton = document.createElement("button");
+
+      //Each element needs modifying
+
+  checkBox.type = "checkbox";
+  editInput.type = "text";
+
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
+
+  label.innerText = taskString;
+
+
+      // each element needs appending
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+}
+
+// Add a new task
+var addTask = function() {
+  console.log("Add task...");
+  //Create a new list item with the text from #new-task:
+  var listItem = createNewTaskElement(taskInput.value);
+  //Append listItem to incompleteTasksHolder
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+
+  taskInput.value = "";
+}
+
+// Edit an existing task
+var editTask = function() {
+  console.log("Edit Task...");
+
+  var listItem = this.parentNode;
+
+  var editInput = listItem.querySelector("input[type=text]")
+  var label = listItem.querySelector("label");
+
+  var containsClass = listItem.classList.contains("editMode");
+    //if the class of the parent is .editMode
+  if(containsClass) {
+      //switch from .editMode
+      //Make label text become the input's value
+    label.innerText = editInput.value;
+    M.toast({html: 'Edit Saved'})
+  } else {
+      //Switch to .editMode
+      //input value becomes the label's text
+    editInput.value = label.innerText;}
+
+    // Toggle .editMode on the parent
+  listItem.classList.toggle("editMode");
+
+}
+
+
+// Delete an existing task
+var deleteTask = function() {
+  console.log("Delete task...");
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
+
+  //Remove the parent list item from the ul
+  ul.removeChild(listItem);
+   M.toast({html: 'Todo Deleted'})
+}
+
+// Mark a task as complete
+var taskCompleted = function() {
+  console.log("Task complete...");
+  M.toast({html: 'Task Completed'})
+  //Append the task list item to the #completed-tasks
+  var listItem = this.parentNode;
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
+}
+
+// Mark a task as incomplete
+var taskIncomplete = function() {
+  console.log("Task Incomplete...");
+  M.toast({html: 'Task Incomplete'})
+  // When checkbox is unchecked
+  // Append the task list item #incomplete-tasks
+  var listItem = this.parentNode;
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+  console.log("Bind list item events");
+  //select taskListItem's children
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
+
+  //bind editTask to edit button
+  editButton.onclick = editTask;
+
+  //bind deleteTask to delete button
+  deleteButton.onclick = deleteTask;
+
+  //bind checkBoxEventHandler to checkbox
+  checkBox.onchange = checkBoxEventHandler;
+}
+
+var ajaxRequest = function() {
+  console.log("AJAX Request");
+}
+
+// Set the click handler to the addTask function
+//addButton.onclick = addTask;
+addButton.addEventListener("click", addTask);
+addButton.addEventListener("click", ajaxRequest);
+
+
+// Cycle over the incompleteTaskHolder ul list items
+for(var i = 0; i <  incompleteTasksHolder.children.length; i++) {
+    // bind events to list item's children (taskCompleted)
+  bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+// Cycle over the completeTaskHolder ul list items
+for(var i = 0; i <  completedTasksHolder.children.length; i++) {
+    // bind events to list item's children (taskIncompleted)
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+
+}
+var input = document.getElementById("new-task");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("myBtn").click();
+  }
+});
+
+window.onload = function() {
+  var chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    theme: 'light2',
+    title: {
+      text: 'Daily Visitors'
+    },
+    axisY: {
+      includeZero: false
+    },
+    data: [
+      {
+        type: 'line',
+        dataPoints: [
+          { y: 450 },
+          { y: 414 },
+          {
+            y: 520,
+            indexLabel: 'highest',
+            markerColor: 'red',
+            markerType: 'triangle'
+          },
+          { y: 460 },
+          { y: 450 },
+          { y: 500 },
+          { y: 480 },
+          { y: 480 },
+          {
+            y: 410,
+            indexLabel: 'lowest',
+            markerColor: 'DarkSlateGrey',
+            markerType: 'cross'
+          },
+          { y: 500 },
+          { y: 480 },
+          { y: 510 }
+        ]
+      }
+    ]
+  });
+  setTimeout(function() {
+    chart.render();
+  }, 1000);
+};
